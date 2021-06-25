4fd1d40dc5f5f65528ba05b664dbfca8c20ab136
ormr <seraf.gavrilov@gmail.com>
2021-06-26 00:10:44 +0300

    next config fix

--- next.config.js
+++ next.config.js
@@ -2,13 +2,5 @@ module.exports = {
   images: {
     domains: ['assets.vercel.com', 'res.cloudinary.com'],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
   distDir: 'out',
 };
